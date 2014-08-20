4e70a2f7f31b128ce6cbcf26beec2e23c4b3cbcc
Paul Lamere <Paul.Lamere@gmail.com>
2014-08-20 16:28:47 -0400

    Fixed up the packaging a bit

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='SpotipyWebApi',
-    version='1.301',
+    version='1.320',
     description='simple client for the Spotify Web API',
     author="@plamere",
     author_email="paul@echonest.com",
     url='http://github.com/plamere/spotipy',
     install_requires=['requests>=1.0'],
     license='LICENSE.txt',
-    py_modules=['spotipy', 'spotipy.oauth2', 'spotipy.util'],)
+    py_modules=['spotipy.spotipy', 'spotipy.oauth2', 'spotipy.util'],)
