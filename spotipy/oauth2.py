0caf56cbf8ad7bdd883d6d91536ad1629c045be2
Joona Hoikkala <joona@mediabox.fi>
2014-09-19 00:27:30 +0300

    Fixes to support both Python 2 & 3, print_function and urllib imports

--- spotipy/oauth2.py
+++ spotipy/oauth2.py
@@ -1,6 +1,12 @@
 
+from __future__ import print_function
 import base64
-import urllib.request, urllib.parse, urllib.error
+# Workaround to support both python 2 & 3
+try:
+    import urllib.request, urllib.error
+    import urllib.parse as urllibparse
+except ImportError:
+    import urllib as urllibparse
 import requests
 import os
 import json
@@ -87,7 +93,7 @@ class SpotifyOAuth(object):
         if self.state:
             payload['state'] = self.state
 
-        urlparams = urllib.parse.urlencode(payload)
+        urlparams = urllibparse.urlencode(payload)
 
         return "%s?%s" % (self.OAUTH_AUTHORIZE_URL, urlparams)
 
