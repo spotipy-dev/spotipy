e71f4ac6d6579e3fb8eb7b01d6c39bfe94eb007b
Joona Hoikkala <joona@mediabox.fi>
2014-11-04 18:09:34 +0200

    Python 3 compatible print syntax to examples

--- examples/show_new_releases.py
+++ examples/show_new_releases.py
@@ -8,8 +8,8 @@ import spotipy.util as util
 if len(sys.argv) > 1:
     username = sys.argv[1]
 else:
-    print "Whoops, need your username!"
-    print "usage: python new_releases.py [username]"
+    print("Whoops, need your username!")
+    print("usage: python new_releases.py [username]")
     sys.exit()
 
 token = util.prompt_for_user_token(username)
@@ -22,11 +22,11 @@ if token:
     while response:
         albums = response['albums']
         for i, item in enumerate(albums['items']):
-            print albums['offset'] + i,item['name']
+            print(albums['offset'] + i,item['name'])
 
         if albums['next']:
             response = sp.next(albums)
         else:
             response = None
 else:
-    print "Can't get token for", username
+    print("Can't get token for", username)
