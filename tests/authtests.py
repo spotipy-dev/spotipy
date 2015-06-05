3d46d7a0355753381397e026dea2d6e9889736ec
Joona Hoikkala <joona@mediabox.fi>
2015-06-05 12:12:34 +0300

    Removed old comment, as the API is fixed in this regard

--- tests/authtests.py
+++ tests/authtests.py
@@ -57,11 +57,6 @@ class AuthTestSpotipy(unittest.TestCase):
     def test_user_playlists(self):
         playlists = spotify.user_playlists(username, limit=5)
         self.assertTrue('items' in playlists)
-
-        # known API issue currently causes this test to fail
-        # the issue is that the API doesn't currently respect the
-        # limit paramter
-
         self.assertTrue(len(playlists['items']) == 5)
 
     def test_user_playlist_tracks(self):
