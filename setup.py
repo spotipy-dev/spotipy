a1501036057140e94bbffcdc6da2859c49316c0f
Paul Lamere <Paul.Lamere@gmail.com>
2017-01-02 12:12:48 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='spotipy',
-    version='2.4.2',
+    version='2.4.3',
     description='simple client for the Spotify Web API',
     author="@plamere",
     author_email="paul@echonest.com",
