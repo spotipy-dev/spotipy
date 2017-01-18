e1344e222111bead760e0c42360bfae53bda348f
Paul Lamere <Paul.Lamere@gmail.com>
2017-01-18 17:35:23 -0500

    Proper requests version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email="paul@echonest.com",
     url='http://spotipy.readthedocs.org/',
     install_requires=[
-        'requests>=1.0',
+        'requests>=2.3.0',
         'six>=1.10.0',
     ],
     license='LICENSE.txt',
