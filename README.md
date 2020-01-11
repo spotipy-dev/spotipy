1d7051c5b0b8ddf60b338ec49a250e154500ae1a
Stephane Bruckert <contact@stephanebruckert.com>
2020-01-11 17:26:11 +0000

    Bump to 2.5.0

--- README.md
+++ README.md
@@ -84,3 +84,4 @@ If you have suggestions, bugs or other issues specific to this library, file the
 - v2.4.2 - January 2, 2017 -- support getting audio features for a single track
 - v2.4.3 - January 2, 2017 -- fixed proxy issue in standard auth flow
 - v2.4.4 - January 4, 2017 -- python 3 fix
+- v2.5.0 - January 11, 2020 -- Added follow and player endpoints
--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name='spotipy',
-    version='2.4.4',
-    description='simple client for the Spotify Web API',
+    version='2.5.0',
+    long_description="""### A light weight Python library for the Spotify Web API""",
+    long_description_content_type='text/markdown',
     author="@plamere",
     author_email="paul@echonest.com",
     url='http://spotipy.readthedocs.org/',
