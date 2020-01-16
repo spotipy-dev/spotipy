07505d720ca27f1f8e8915b13c5779bfbf600df3
Stephane Bruckert <contact@stephanebruckert.com>
2020-01-16 04:14:42 +0000

    Bump to 2.6.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [2.6.2] - 2020-01-16
+
 ### Fixed
 
  - Fixed broken examples in README, examples and doc
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ desc = """### A light weight Python library for the Spotify Web API"""
 
 setup(
     name='spotipy',
-    version='2.6.1',
+    version='2.6.2',
     long_description=desc,
     long_description_content_type='text/markdown',
     author="@plamere",
