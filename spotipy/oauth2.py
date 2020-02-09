4396c8cdad4ba4fe67b63ca50ef1477bd885c3b5
Sandeep Murthy <smurthy@protonmail.ch>
2020-02-09 09:27:19 +0000

    Remove `f`-string from OAuth2 mod.

--- spotipy/oauth2.py
+++ spotipy/oauth2.py
@@ -29,7 +29,7 @@ class SpotifyOauthError(Exception):
 def _make_authorization_headers(client_id, client_secret):
     auth_header = base64.b64encode(
         six.text_type(
-            f'{client_id}:{client_secret}'
+            '{}:{}'.format(client_id, client_secret)
         ).encode('ascii'))
     return {'Authorization': 'Basic %s' % auth_header.decode('ascii')}
 
