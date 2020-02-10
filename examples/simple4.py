c3681d0dda6a15f9b84933c11facf7e0c50aa169
stefano <frassetto.stefano@gmail.com>
2020-02-10 10:43:02 +0100

    Update simple4 example.

--- examples/simple4.py
+++ examples/simple4.py
@@ -2,15 +2,8 @@ import spotipy
 import os
 from pprint import pprint
 
-
 def main():
-    auth_manager = spotipy.SpotifyOAuth(
-        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
-        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
-        redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
-        username=os.getenv("SPOTIPY_CLIENT_USERNAME"),
-    )
-    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    spotify = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth())
     me = spotify.me()
     pprint(me)
 
