60f530842d8f20e074fc12007fbf25c08e388267
Stephane Bruckert <contact@stephanebruckert.com>
2020-02-12 23:14:26 +0000

    Fix

--- examples/playlist_tracks.py
+++ examples/playlist_tracks.py
@@ -5,13 +5,15 @@ from pprint import pprint
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 pl_id = 'spotify:playlist:5RIbzhG2QqdkaP24iXLnZX'
-response = sp.playlist_tracks(pl_id, fields='items.track.id,total')
 offset = 0
 
-while len(response['items']) > 0:
-    print(offset, "/", response['total'])
+while True:
     response = sp.playlist_tracks(pl_id,
                                   offset=offset,
                                   fields='items.track.id,total')
-    offset = offset + len(response['items'])
     pprint(response['items'])
+    offset = offset + len(response['items'])
+    print(offset, "/", response['total'])
+
+    if len(response['items']) == 0:
+        break
