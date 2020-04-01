ceb916721ac21a8b4061bbdb8c8e88efe5902650
Dj <Beahmer89@gmail.com>
2020-04-01 08:04:14 -0700

    Logging - add logging lines to oauth

--- spotipy/oauth2.py
+++ spotipy/oauth2.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-
 __all__ = [
     "is_token_expired",
     "SpotifyClientCredentials",
@@ -11,10 +9,11 @@ __all__ = [
 
 import base64
 import json
+import logging
 import os
-import sys
 import time
 import warnings
+import webbrowser
 
 import requests
 from spotipy.util import CLIENT_CREDS_ENV_VARS, get_host_port
@@ -25,6 +24,8 @@ import six
 import six.moves.urllib.parse as urllibparse
 from six.moves.BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from six.moves.urllib_parse import urlparse, parse_qsl
+
+LOGGER = logging.getLogger(__name__)
 
 
 class SpotifyOauthError(Exception):
@@ -273,7 +274,8 @@ class SpotifyOAuth(SpotifyAuthBase):
                 f.write(json.dumps(token_info))
                 f.close()
             except IOError:
-                self._warn("couldn't write token cache to " + self.cache_path)
+                LOGGER.warning('Couldn\'t write token to cache at: %s',
+                               self.cache_path)
 
     def _is_scope_subset(self, needle_scope, haystack_scope):
         needle_scope = set(needle_scope.split()) if needle_scope else set()
@@ -323,25 +325,19 @@ class SpotifyOAuth(SpotifyAuthBase):
 
     def _open_auth_url(self):
         auth_url = self.get_authorize_url()
-
         try:
-            import webbrowser
             webbrowser.open(auth_url)
-            print("Opened %s in your browser" % auth_url)
+            LOGGER.info("Opened %s in your browser", auth_url)
         except webbrowser.Error:
-            print("Please navigate here: %s" % auth_url)
+            LOGGER.error("Please navigate here: %s", auth_url)
 
     def _get_auth_response_interactive(self):
         self._open_auth_url()
-        print("")
-        print("")
-
         try:
             response = raw_input("Enter the URL you were redirected to: ")
         except NameError:
             response = input("Enter the URL you were redirected to: ")
-        print("")
-        print("")
+
         return self.parse_response_code(response)
 
     def _get_auth_response_local_server(self, redirect_port):
@@ -357,16 +353,11 @@ class SpotifyOAuth(SpotifyAuthBase):
             raise SpotifyOauthError("Server listening on localhost has not been accessed")
 
     def get_auth_response(self):
-        print(
-            """
-
-            User authentication requires interaction with your
-            web browser. Once you enter your credentials and
-            give authorization, you will be redirected to
-            a url.
-
-        """
-        )
+        LOGGER.info('User authentication requires interaction with your '
+                    'web browser. Once you enter your credentials and '
+                    'give authorization, you will be redirected to '
+                    'a url.  Paste that url you were directed to to '
+                    'complete the authorization.')
 
         redirect_info = urlparse(self.redirect_uri)
         redirect_host, redirect_port = get_host_port(redirect_info.netloc)
@@ -374,10 +365,8 @@ class SpotifyOAuth(SpotifyAuthBase):
         if redirect_host in ("127.0.0.1", "localhost") and redirect_info.scheme == "http":
             return self._get_auth_response_local_server(redirect_port)
         else:
-            print("""
-            Paste that url you were directed to to
-            complete the authorization.
-""")
+            LOGGER.info('Paste that url you were directed to in order to '
+                        'complete the authorization')
             return self._get_auth_response_interactive()
 
     def get_authorization_code(self, response=None):
@@ -393,7 +382,6 @@ class SpotifyOAuth(SpotifyAuthBase):
                             as a string.
         """
         if as_dict:
-            print("")
             warnings.warn(
                 "You're using 'as_dict = True'."
                 "get_access_token will return the token string directly in future "
@@ -402,7 +390,6 @@ class SpotifyOAuth(SpotifyAuthBase):
                 DeprecationWarning,
                 stacklevel=2,
             )
-            print("")
         if check_cache:
             token_info = self.get_cached_token()
             if token_info is not None:
@@ -461,9 +448,12 @@ class SpotifyOAuth(SpotifyAuthBase):
             proxies=self.proxies,
             timeout=self.requests_timeout,
         )
+
         try:
             response.raise_for_status()
         except BaseException:
+            LOGGER.warning('Couldn\'t refresh token. Response STATUS CODE: %s '
+                           'REASON: %s', response.status_code, response.reason)
             message = "Couldn't refresh token: code:%d reason:%s" % (
                 response.status_code,
                 response.reason,
@@ -472,6 +462,7 @@ class SpotifyOAuth(SpotifyAuthBase):
                                    -1,
                                    message,
                                    headers)
+
         token_info = response.json()
         token_info = self._add_custom_values_to_token_info(token_info)
         if "refresh_token" not in token_info:
@@ -487,9 +478,6 @@ class SpotifyOAuth(SpotifyAuthBase):
         token_info["expires_at"] = int(time.time()) + token_info["expires_in"]
         token_info["scope"] = self.scope
         return token_info
-
-    def _warn(self, msg):
-        print("warning:" + msg, file=sys.stderr)
 
 
 class RequestHandler(BaseHTTPRequestHandler):
