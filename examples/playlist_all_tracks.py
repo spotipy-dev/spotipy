cea304d6a86a5dca257ab979f22ba0ae07404d8d
Omglolyes <58954125+Omglolyes@users.noreply.github.com>
2020-06-25 10:19:21 +0200

    Update playlist_all_tracks.py
    forgot first items from playlist

--- examples/playlist_all_tracks.py
+++ examples/playlist_all_tracks.py
@@ -5,9 +5,17 @@ from pprint import pprint as pp
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
+# load the first 100 songs
+tracks = []
+result = sp.playlist_tracks()
+tracks.extend(result['items'])
+
+# if playlist is larger than 100 song, continue loading it until end
 while result['next']:
 	result = sp.next(result)
 	tracks.extend(result['items'])
+
+# remove all local songs
 for item in tracks:
 	if item["is_local"] == True:
 		tracks.remove(item)
