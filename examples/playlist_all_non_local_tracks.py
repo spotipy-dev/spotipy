e3855e75d3008e0bcae906ec6bdf1e407d36b609
Omglolyes <58954125+Omglolyes@users.noreply.github.com>
2020-06-29 20:17:17 +0200

    Update playlist_all_non_local_tracks.py
    removed "== True" as it's implied
    give an example playlist (global top 50)
    more comments
    corrected typo

--- examples/playlist_all_non_local_tracks.py
+++ examples/playlist_all_non_local_tracks.py
@@ -1,23 +1,28 @@
 # get all non-local tracks of a playlist
+from pprint import pprint as pp
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
-from pprint import pprint as pp
 
+# playlist id of global top 50
+PlaylistExample = '37i9dQZEVXbMDoHDwVN2tF'
+
+# create spotipy client
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 # load the first 100 songs
 tracks = []
-result = sp.playlist_tracks()
+result = sp.playlist_tracks(PlaylistExample)
 tracks.extend(result['items'])
 
-# if playlist is larger than 100 song, continue loading it until end
+# if playlist is larger than 100 songs, continue loading it until end
 while result['next']:
 	result = sp.next(result)
 	tracks.extend(result['items'])
 
 # remove all local songs
 for item in tracks:
-	if item["is_local"] == True:
+	if item['is_local']:
 		tracks.remove(item)
-		
+
+# print result
 pp(tracks)
