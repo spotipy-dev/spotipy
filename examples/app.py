450fc98dd519c57f1cd0799e4ea5e44d23c4a10c
Mike iLL Kilmer <mike@mzoo.org>
2020-07-26 05:54:09 -0500

    Add a second scope and also a view which utilizes a scope.

--- examples/app.py
+++ examples/app.py
@@ -7,7 +7,7 @@ Prerequisites
     export SPOTIPY_CLIENT_ID=client_id_here
     export SPOTIPY_CLIENT_SECRET=client_secret_here
     export SPOTIPY_REDIRECT_URI='http://127.0.0.1:8080' // must contain a port
-
+    
     // on Windows, use `SET` instead of `export`
 
 Run app.py
@@ -37,7 +37,7 @@ def index():
         # Step 1. Visitor is unknown, give random ID
         session['uuid'] = str(uuid.uuid4())
 
-    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing',
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
                                                 cache_path=session_cache_path(), 
                                                 show_dialog=True)
     if request.args.get("code"):
@@ -54,7 +54,8 @@ def index():
     spotify = spotipy.Spotify(auth_manager=auth_manager)
     return f'<h2>Hi {spotify.me()["display_name"]}, ' \
            f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
-           f'<a href="/playlists">my playlists</a>'
+           f'<a href="/playlists">my playlists</a> | ' \
+           f'<a href="/currently_playing">currently playing</a>'
 
 
 @app.route('/sign_out')
@@ -74,6 +75,17 @@ def playlists():
     spotify = spotipy.Spotify(auth_manager=auth_manager)
     return spotify.current_user_playlists()
 
-
 def session_cache_path():
     return caches_folder + session.get('uuid')
+    
+@app.route('/currently_playing')
+def currently_playing():
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=session_cache_path())
+
+    if not auth_manager.get_cached_token():
+        return redirect('/')
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    track = spotify.current_user_playing_track()
+    if not track is None:
+        return track
+    return "No track currently playing."
