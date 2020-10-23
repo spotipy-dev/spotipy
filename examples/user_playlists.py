1e831c627d4587765b46e819ee5f65b0be75ca00
Stephane Bruckert <stephane.bruckert@gmail.com>
2020-10-23 22:52:29 +0100

    Fix example to use user_playlists again

--- examples/user_playlists.py
+++ examples/user_playlists.py
@@ -1,11 +1,19 @@
 # Shows a user's playlists (need to be authenticated via oauth)
 
+import sys
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = 'playlist-read-private'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-playlists = sp.current_user_playlists()
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print("Whoops, need a username!")
+    print("usage: python user_playlists.py [username]")
+    sys.exit()
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth())
+
+playlists = sp.user_playlists(username)
 
 for playlist in playlists['items']:
     print(playlist['name'])
