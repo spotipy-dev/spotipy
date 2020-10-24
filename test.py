b68a70221457951d18321773936ddcf7a5c49795
Stephane Bruckert <stephane.bruckert@gmail.com>
2020-10-24 02:49:48 +0100

    Lint

--- test.py
+++
@@ -1,8 +0,0 @@
-import spotipy
-from pprint import pprint
-# Spotify init
-sp = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(scope='playlist-modify-public playlist-modify-private',
-                                                       show_dialog=True))
-playlist = sp.album("0sNOF9WDwhWunNAHPD3Baj")
-
-pprint(playlist['label'])
