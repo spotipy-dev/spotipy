54ec28a2dc26488aae9e2e4690eaa630d43b2743
Rob Sanek <rsanek@gmail.com>
2020-11-10 10:07:11 -0800

    Add market argument to track info
    so that we know if a track is available to be played in a specific country

--- spotipy/client.py
+++ spotipy/client.py
@@ -327,7 +327,7 @@ class Spotify(object):
         else:
             return None
 
-    def track(self, track_id):
+    def track(self, track_id, market=None):
         """ returns a single track given the track's ID, URI or URL
 
             Parameters:
@@ -335,7 +335,7 @@ class Spotify(object):
         """
 
         trid = self._get_id("track", track_id)
-        return self._get("tracks/" + trid)
+        return self._get("tracks/" + trid, market=market)
 
     def tracks(self, tracks, market=None):
         """ returns a list of tracks given a list of track IDs, URIs, or URLs
