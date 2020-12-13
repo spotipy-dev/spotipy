5469db58f3511d384dd187c6605ea76f73e7755f
Joshua Baunach <joshua.baunach@gmail.com>
2020-12-12 21:49:32 -0500

    Fix examples for saved tracks
    This fixes the examples showin in delete_a_saved_track.py and contains_a_saved_track.py

--- examples/contains_a_saved_track.py
+++ examples/contains_a_saved_track.py
@@ -1,3 +1,5 @@
+# Prints whether a track exists in your collection of saved tracks
+
 import pprint
 import sys
 
@@ -7,11 +9,11 @@ from spotipy.oauth2 import SpotifyOAuth
 scope = 'user-library-read'
 
 if len(sys.argv) > 1:
-    tids = sys.argv[1]
+    tid = sys.argv[1]
 else:
     print("Usage: %s track-id ..." % (sys.argv[0],))
     sys.exit()
 
-    results = spotipy.current_user_saved_tracks_contains(tracks=tids)
-    pprint.pprint(results)
-
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+results = sp.current_user_saved_tracks_contains(tracks=[tid])
+pprint.pprint(results)
