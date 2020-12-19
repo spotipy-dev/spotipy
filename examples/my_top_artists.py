7891fc63cb6c6fad1bd47de797b75041fe155346
rohan-ynwa <70969245+rohan-ynwa@users.noreply.github.com>
2020-12-19 15:20:37 -0500

    Update my_top_artists.py

--- examples/my_top_artists.py
+++ examples/my_top_artists.py
@@ -1,17 +1,26 @@
-# Shows the top artists for a user
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = 'user-top-read'
+scope = "user-library-read user-top-read playlist-modify-public"
 ranges = ['short_term', 'medium_term', 'long_term']
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = "326a452a53254fa9bc90368567c70861", client_secret = "7d518966cb804f019a4301db077c1c31", redirect_uri = "http://localhost:8888/callback", scope = scope))
 
-for sp_range in ['short_term', 'medium_term', 'long_term']:
-    print("range:", sp_range)
+# Shows the top artists for a user
+def get_top_artists(sp):
+    
+    top_artists = sp.current_user_top_artists(time_range = "short_term", limit = 15)
+    return top_artists['items']
 
-    results = sp.current_user_top_artists(time_range=sp_range, limit=50)
-
-    for i, item in enumerate(results['items']):
-        print(i, item['name'])
-    print()
+# gets top tracks for the user
+def get_top_tracks(sp, top_artists):
+    
+    top_tracks_uri = []
+    for i in top_artists:
+        top_tracks_all_data = sp.artist_top_tracks(i)
+        top_tracks_data = top_tracks_all_data['tracks']
+        for track_data in top_tracks_data:
+            top_tracks_uri.append(track_data['uri'])
+    
+    return top_tracks_uri
