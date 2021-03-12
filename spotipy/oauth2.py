690f389782c0609dc1e3b9f33d828496f20390af
Peter-Schorn <petervschorn@gmail.com>
2021-03-12 05:55:48 -0600

    Removed unneeded import

--- spotipy/oauth2.py
+++ spotipy/oauth2.py
@@ -24,7 +24,6 @@ from six.moves.BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from six.moves.urllib_parse import parse_qsl, urlparse
 
 from spotipy.cache_handler import CacheFileHandler, CacheHandler
-from spotipy.exceptions import SpotifyException
 from spotipy.util import CLIENT_CREDS_ENV_VARS, get_host_port, normalize_scope
 
 logger = logging.getLogger(__name__)
