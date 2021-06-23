0047299e555e6d585b40ace9cf9a42eaad1a4be2
Daniel Espino <akathorn@gmail.com>
2021-06-23 11:14:43 +0200

    Add typing_extensions as dependency

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     author_email="paul@echonest.com",
     url='https://spotipy.readthedocs.org/',
     install_requires=[
+        'typing_extensions>=3.10.0',
         'requests>=2.25.0',
         'six>=1.15.0',
         'urllib3>=1.26.0'
