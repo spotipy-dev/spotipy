da1e47ef2e8084e50d31e32664a86c82cee0bb12
Aksana Rautskaya <rautskaa@oregonstate.edu>
2021-11-23 13:35:12 -0800

    Renaming to unfollow_playlist

--- examples/unfollow_playlist.py
+++ examples/unfollow_playlist.py
@@ -4,12 +4,18 @@ import logging
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-logger = logging.getLogger('examples.delete_playlist')
+logger = logging.getLogger('examples.unfollow_playlist')
 logging.basicConfig(level='DEBUG')
+
+'''
+Spotify doesn't have a dedicated endpoint for deleting a playlist. However,
+unfollowing a playlist has the effect of deleting it from the user's account.
+When a playlist is removed from the user's account, the system unfollows it,
+and then no longer shows it in playlist list.'''
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Deletes a playlist for user')
+    parser = argparse.ArgumentParser(description='Unfollows a playlist')
     parser.add_argument('-p', '--playlist', required=True,
                         help='Playlist id')
     return parser.parse_args()
