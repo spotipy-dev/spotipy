24921ca5f2f1588637dcf98661a30d1d2c2407ff
Felix Cusson <60357894+Darkfull-Dante@users.noreply.github.com>
2021-12-16 11:30:20 -0500

    made _get_id take account of ? in URI

--- spotipy/client.py
+++ spotipy/client.py
@@ -12,6 +12,8 @@ import requests
 import six
 import urllib3
 
+import re
+
 from spotipy.exceptions import SpotifyException
 
 logger = logging.getLogger(__name__)
@@ -1913,7 +1915,7 @@ class Spotify(object):
             if type != fields[-2]:
                 logger.warning('Expected id of type %s but found type %s %s',
                                type, fields[-2], id)
-            return fields[-1]
+            return fields[-1].spliy("?")[0]
         fields = id.split("/")
         if len(fields) >= 3:
             itype = fields[-2]
