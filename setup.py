55e5cd473ca2accc11b13eb0dc9cada2f5155d2b
Stéphane Bruckert <contact@stephanebruckert.com>
2022-06-18 23:21:17 +0100

    Don't update version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ extra_reqs = {
 
 setup(
     name='spotipy',
-    version='2.19.1',
+    version='2.19.0',
     description='A light weight Python library for the Spotify Web API',
     long_description=long_description,
     long_description_content_type="text/markdown",
