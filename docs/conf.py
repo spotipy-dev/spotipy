126da62dd0646a3cc685c9a3d3dd1eabde588117
Niko <github@dieserniko.link>
2024-05-30 20:33:17 +0200

    Fix API Reference (#1126)
    * add base directory to PATH
    
    * Check if build completes if redis is included

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,9 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath(".."))
+
+import spotipy
 
 # -- General configuration -----------------------------------------------------
 
