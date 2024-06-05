6ef01c4b3246d181d5f854cd385310414f09602c
dianna-SE <diannapham.se@gmail.com>
2024-06-05 16:58:58 -0700

    Added and revised function docstrings for util.py

--- spotipy/util.py
+++ spotipy/util.py
@@ -1,4 +1,4 @@
-""" Shows a user's playlists (need to be authenticated via oauth) """
+""" Shows a user's playlists. This needs to be authenticated via OAuth) """
 
 __all__ = ["CLIENT_CREDS_ENV_VARS", "prompt_for_user_token"]
 
@@ -35,21 +35,28 @@ def prompt_for_user_token(
         "    spotipy.Spotify(auth_manager=auth_manager)",
         DeprecationWarning
     )
-    """ prompts the user to login if necessary and returns
-        the user token suitable for use with the spotipy.Spotify
-        constructor
+    """Prompt the user to login if necessary and return a user token.
 
-        Parameters:
+        Prompts the user to login if necessary and returns the user token
+        suitable for use with the `spotipy.Spotify` constructor.
 
-         - username - the Spotify username (optional)
-         - scope - the desired scope of the request (optional)
-         - client_id - the client id of your app (required)
-         - client_secret - the client secret of your app (required)
-         - redirect_uri - the redirect URI of your app (required)
-         - cache_path - path to location to save tokens (optional)
-         - oauth_manager - Oauth manager object (optional)
-         - show_dialog - If true, a login prompt always shows (optional, defaults to False)
+        Args:
+            username: The Spotify username. (optional)
+            scope: The desired scope of the request. (optional)
+            client_id: The client ID of your app. (required)
+            client_secret: The client secret of your app. (required)
+            redirect_uri: The redirect URI of your app. (required)
+            cache_path: Path to location to save tokens. (required)
+            oauth_manager: OAuth manager object. (optional)
+            show_dialog: If True, a login prompt always shows.
+                         If not, it will default to False. (optional)
 
+        Returns:
+            A token suitable for use with the `spotipy.Spotify` constructor.
+            None: If no valid token is found.
+
+        Raises:
+            SpotifyException: If no credentials are set.
     """
     if not oauth_manager:
         if not client_id:
@@ -91,7 +98,9 @@ def prompt_for_user_token(
     # if not in the cache, then create a new (this will send
     # the user to a web page where they can authorize this app)
 
-    token_info = sp_oauth.validate_token(sp_oauth.cache_handler.get_cached_token())
+    token_info = sp_oauth.validate_token(
+        sp_oauth.cache_handler.get_cached_token()
+    )
 
     if not token_info:
         code = sp_oauth.get_auth_response()
@@ -107,6 +116,16 @@ def prompt_for_user_token(
 
 
 def get_host_port(netloc):
+    """Split the network location string into host and port.
+
+    Args:
+        netloc: A string representing the network location.
+
+    Returns:
+        A tuple where the first element is the host as a string,
+        and the second element is the port as an integer.
+        None if no port is specified.
+    """
     if ":" in netloc:
         host, port = netloc.split(":", 1)
         port = int(port)
@@ -118,6 +137,25 @@ def get_host_port(netloc):
 
 
 def normalize_scope(scope):
+    """Normalize the scope to verify that it is a list or tuple.
+
+    If the input is a string, the function splits the string by commas to
+    create a list of scopes.
+    If the input is already a list or tuple. it is used directly.
+
+    Args:
+        scope: A string representing scopes separated by commas,
+        or a list/tuple of scopes.
+
+    Returns:
+        A string of sorted scopes separated by spaces if
+        input is valid, otherwise None.
+
+    Raises:
+        Exception: If the scope is not a string, list, or tuple.
+        The scope value must be a list of scopes or a string of scopes
+        separated by commas.
+    """
     if scope:
         if isinstance(scope, str):
             scopes = scope.split(',')
@@ -125,8 +163,8 @@ def normalize_scope(scope):
             scopes = scope
         else:
             raise Exception(
-                "Unsupported scope value, please either provide a list of scopes, "
-                "or a string of scopes separated by commas"
+                "Unsupported scope value, please either provide"
+                "a list of scopes, or a string of scopes separated by commas"
             )
         return " ".join(sorted(scopes))
     else:
