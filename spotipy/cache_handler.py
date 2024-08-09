2cd627c265d08e3b74943cfa9757657f3fd24f0f
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2024-08-09 14:08:08 -0500

    fixed code style

--- spotipy/cache_handler.py
+++ spotipy/cache_handler.py
@@ -21,6 +21,7 @@ logger = logging.getLogger(__name__)
 
 TokenInfoType = Dict[str, Union[str, int]]
 
+
 class CacheHandler(ABC):
     """
     An abstraction layer for handling the caching and retrieval of
@@ -211,6 +212,7 @@ class RedisCacheHandler(CacheHandler):
 class MemcacheCacheHandler(CacheHandler):
     """A Cache handler that stores the token info in Memcache using the pymemcache client
     """
+
     def __init__(self, memcache, key=None) -> None:
         """
         Parameters:
