aa2c5b47cb1f81a7835c344d8fed9323337e48af
Robin-St <robin@farsk.net>
2024-11-23 10:28:29 +0100

    Troubleshooting aid for headless mode

--- examples/headless.py
+++ examples/headless.py
@@ -3,6 +3,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 # set open_browser=False to prevent Spotipy from attempting to open the default browser
+# Note that some browsers might hide the URL redirected to, if this happens test another browser
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(open_browser=False))
 
 print(spotify.me())
